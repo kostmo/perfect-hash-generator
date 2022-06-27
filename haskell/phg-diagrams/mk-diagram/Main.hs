5d861b7e6db012c815fc47ee7b37e94b45837edd
Karl Ostmo <kostmo@gmail.com>
2022-06-27 00:01:36 -0700

    diagram

--- haskell/phg-diagrams/mk-diagram/Main.hs
+++ haskell/phg-diagrams/mk-diagram/Main.hs
@@ -2,28 +2,9 @@
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE TypeFamilies              #-}
 
-import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
-singleSquare :: String -> Diagram B
-singleSquare val = writing <> enclosure
-  where
-    enclosure = square 40 # fc blue
-    writing = text val # fontSizeL 20 # fc white
-
-
-diagramLeft = vcat [
-      singleSquare "foo"
-    , singleSquare "bar"
-    , singleSquare ""
-    , singleSquare "blah"
-    , singleSquare ""
-    ]
-
-
-diagramRight = vcat (replicate 5 $ singleSquare $ show 3)
-
-myDiagram = hcat [diagramLeft, diagramRight]
+import DiagramsHelper
 
 
 main = mainWith myDiagram
--- haskell/phg-diagrams/src/DiagramsHelper.hs
+++ haskell/phg-diagrams/src/DiagramsHelper.hs
@@ -4,4 +4,43 @@
 
 module DiagramsHelper where
 
--- import Diagrams.Prelude
+import Diagrams.Prelude
+import Diagrams.Backend.SVG.CmdLine
+import Data.Colour.Palette.BrewerSet
+
+
+colors :: [Kolor]
+colors = brewerSet Pastel1 9
+
+
+textEnvelope val = text val # fontSizeL 15 # withEnvelope (rect 120 30 :: Diagram B)
+
+
+singleSquare :: Colour Double -> String -> Diagram B
+singleSquare colr val = writing <> enclosure
+  where
+    enclosure = roundedRect 40 30 5 # fc colr # lw none
+    writing = text val # fontSizeL 20 # fc black
+
+
+diagramLeft = vcat [vsep 10 [
+      sqr "foo"
+    , sqr "bar"
+    , sqr ""
+    , sqr "blah"
+    , sqr ""
+    ]
+  , textEnvelope "intermediate table"
+  ]
+  where
+    sqr = singleSquare (colors !! 4)
+
+
+diagramRight = vcat [
+    vsep 10 items
+  , textEnvelope "value table"
+  ]
+  where
+    items = replicate 5 $ singleSquare (colors !! 3) $ show 3
+
+myDiagram = hsep 20 [diagramLeft, diagramRight]
